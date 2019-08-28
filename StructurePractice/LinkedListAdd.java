f81a8368e1d6cd85c54d894789d50b5966a0a400
neel <neel@10.1.10.3>
2019-08-28 13:50:00 -0400

    debugging linkedlistadd

--- StructurePractice/LinkedListAdd.java
+++ StructurePractice/LinkedListAdd.java
@@ -2,13 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
-
+/*
+ * Adds two files num1 and num2 containing integers using linked lists
+ * @author Neel Sortur
+ */
 public class LinkedListAdd {
 
 	public static Node head;
 
 	public static Node curr;
-	private int val;
+	
+	public static LinkedListAdd listOne;
+	public static LinkedListAdd listTwo;
 
 	public static void main(String[] args) {
 		Scanner numOne = null;
@@ -20,15 +25,21 @@ public class LinkedListAdd {
 			e.printStackTrace();
 		}
 
-		LinkedListAdd listOne = new LinkedListAdd(numOne.nextInt());
+		listOne = new LinkedListAdd(numOne.nextInt());
 		addToList(listOne, numOne);
 		
-		LinkedListAdd listTwo = new LinkedListAdd(numTwo.nextInt());
-		addToList(listTwo, numTwo);
+//		listTwo = new LinkedListAdd(numTwo.nextInt());
+//		addToList(listTwo, numTwo);
 		
+		printList(listOne);
+		
+		//LinkedListAdd prnt = addLists(listOne, listTwo);
+		
+		//printList(prnt);
 		
 	}
-
+	public LinkedListAdd() {head = new Node();}
+	
 	public LinkedListAdd(int value) {
 		head = new Node(value);
 		curr = head;
@@ -52,5 +63,34 @@ public class LinkedListAdd {
 			System.out.println(list.curr.getValue());
 			list.curr = list.curr.getNext();
 		}
+	}
+	
+	public static LinkedListAdd addLists(LinkedListAdd first, LinkedListAdd second) {
+		int carry = 0;
+		int val;
+		LinkedListAdd result = new LinkedListAdd();
+		
+		//sets pointers to last node in linked list
+		while(first.curr.getNext() != null) {
+			first.curr = first.curr.getNext();
+		}
+		while(second.curr.getNext() != null) {
+			second.curr = second.curr.getNext();
+		}
+		System.out.println(first.curr.getValue());
+		
+//		while(first.curr != null && second.curr != null) {
+//			val = first.curr.getValue() + second.curr.getValue() + carry;
+//			//gets carried value
+////			if(val > 9) {
+////				carry = val % 10;
+////				val -= 10;
+////			}
+////			result.curr.setValue(val);
+////			
+////			first.curr = first.curr.getPrev();
+////			second.curr = second.curr.getPrev();
+//		}
+		return result;
 	}
 }
